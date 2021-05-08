164bd5926709e02c77d672a14d392ed84fcd8042
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-08 20:32:06 -0300

    Create Stf AboutViewWidget without Scaffold

---
+++ lib/views/about_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutViewWidget extends StatefulWidget {
+  @override
+  AboutViewWidgetState createState() => AboutViewWidgetState();
+}
+
+class AboutViewWidgetState extends State<AboutViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Message Text"),
+    );
+  }
+}
