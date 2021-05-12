65d45c94d4baf91f8c9a1e1ea1694806a082be6c
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-05-12 12:50:19 -0300

    Import staggered_grid_view

--- lib/views/about_view.dart
+++ lib/views/about_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class AboutViewWidget extends StatefulWidget {
   @override
